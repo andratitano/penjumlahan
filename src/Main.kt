997477209f1b45b574d9e9b37b37ca2643c0fa21
andratitano <andrabusra10@gmail.com>
2020-07-01 09:50:00 +0700

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+    fun main() {
+
+        val bil1: Int = 9
+        val bil2: Int = 6
+
+        val sum = bil1 + bil2
+
+        println("Hasil penjumlahan kedua bilangan tersebut adalah: $sum")
+    }
